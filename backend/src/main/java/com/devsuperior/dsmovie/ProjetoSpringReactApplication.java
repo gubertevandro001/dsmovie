dac62dd31b434d8b08475cbb4b17f3fe14fbadc2
Evandro Gubert <gubertevandro33@gmail.com>
2022-01-12 21:01:18 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/dsmovie/ProjetoSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringReactApplication.class, args);
+	}
+
+}
